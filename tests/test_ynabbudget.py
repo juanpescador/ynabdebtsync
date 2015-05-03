00eed59e61db0097e2c7654e16fa2eb6a417eefe
John Fisher <juanpescador@users.noreply.github.com>
2015-05-04 01:38:55 +0200

    Check for None, empty and malformed budget
    Add custom exception for malformed budgets.

--- tests/test_ynabbudget.py
+++ tests/test_ynabbudget.py
@@ -1,8 +1,9 @@
 # -*- coding: utf8 -*-
 import os
+from nose.tools import assert_raises, assert_equal
 from ynabdebtsync import ynabbudget
 
-def test_loading_budget_works():
+def test_instantiating_budget_with_json_works():
     budget_file_path = os.path.join(os.path.dirname(__file__), "budget.json")
 
     with open (budget_file_path) as budget_file:
@@ -10,3 +11,21 @@ def test_loading_budget_works():
     ynab_budget = ynabbudget.YnabBudget(budget_json)
 
     assert ynab_budget.data["fileMetaData"]["budgetDataVersion"] == "4.2"
+
+def test_instantiating_budget_without_json_raises_exception():
+    with assert_raises(ynabbudget.YnabBudgetMalformedError) as e:
+        ynab_budget = ynabbudget.YnabBudget(None)
+
+    assert_equal(e.exception.message, "Budget JSON is None")
+
+def test_instantiating_budget_with_empty_json_raises_exception():
+    with assert_raises(ynabbudget.YnabBudgetMalformedError) as e:
+        ynab_budget = ynabbudget.YnabBudget("")
+
+    assert_equal(e.exception.message, "Budget JSON is empty")
+
+def test_instantiating_budget_with_malformed_json_raises_exception():
+    with assert_raises(ynabbudget.YnabBudgetMalformedError) as e:
+        ynab_budget = ynabbudget.YnabBudget("[")
+
+    assert_equal(e.exception.message, "Budget JSON is malformed")
--- ynabdebtsync/ynabbudget.py
+++ ynabdebtsync/ynabbudget.py
@@ -4,4 +4,26 @@ import json
 
 class YnabBudget:
     def __init__(self, budget_json):
-        self.data = json.loads(budget_json)
+        if budget_json is None:
+            raise YnabBudgetMalformedError("Budget JSON is None")
+        if budget_json == "":
+            raise YnabBudgetMalformedError("Budget JSON is empty")
+        try:
+            self.data = json.loads(budget_json)
+        except ValueError as e:
+            raise YnabBudgetMalformedError("Budget JSON is malformed",
+                                           e,
+                                           budget_json)
+
+class Error(Exception):
+    """Base class for YNAB budget errors."""
+    pass
+
+class YnabBudgetMalformedError(Error):
+    """Exception raised when the YNAB budget JSON is malformed."""
+    def __init__(self, message="", inner_message="", budget_json=""):
+        super(Error, self).__init__(message)
+
+        self.message = message
+        self.inner_message = inner_message
+        self.budget_json = budget_json
