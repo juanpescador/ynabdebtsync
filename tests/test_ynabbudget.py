09ecb0a43108575d74042fc3947c4e12eac52e09
John Fisher <juanpescador@users.noreply.github.com>
2015-05-28 01:07:09 +0200

    Fix missing transaction to test for
    The real missing transaction from this_budget is other_budget's "Loan
    for beer". "Borrow for nachos" is other_budget's pair transaction for
    this_budget's "Loan for nachos".

--- tests/test_ynabbudget.py
+++ tests/test_ynabbudget.py
@@ -105,8 +105,8 @@ def test_get_missing_transactions_of_amount_returns_correct_missing_transactions
     budget_comparer = ynabbudget.YnabBudgetComparer(this_budget_json, "Test Debt Category", other_budget_json, "Test Debt Category")
 
     missing_transaction = budget_comparer._get_missing_transactions_of_amount(5)[0]
-    assert_equal(missing_transaction["memo"], "Borrow for nachos")
-    assert_equal(missing_transaction["amount"], 5)
+    assert_equal(missing_transaction["memo"], "Loan for beer")
+    assert_equal(missing_transaction["amount"], -5)
 
 def test_no_transactions_of_given_amount_throws_exception():
     budget_comparer = ynabbudget.YnabBudgetComparer(this_budget_json, "Test Debt Category", other_budget_json, "Test Debt Category")
