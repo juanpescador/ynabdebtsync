e142097fd682c76eecbe436e545762015686e433
John Fisher <juanpescador@users.noreply.github.com>
2015-06-01 23:26:51 +0200

    Indent custom transactions

--- tests/test_ynabbudget.py
+++ tests/test_ynabbudget.py
@@ -167,29 +167,50 @@ def test_get_missing_transactions_this_transactions_other_transactions_equal_len
 def test_get_missing_transactions_this_negative_gt_other_positive_returns_this_transaction():
     budget_comparer = ynabbudget.YnabBudgetComparer(this_budget_json, "Test Debt Category", other_budget_json, "Test Debt Category")
 
-    this_transactions = [{u'cleared': u'Cleared',
-                     u'entityVersion': u'A-118',
-                     u'entityType': u'transaction',
-                     u'memo': u'Loan for tea',
-                     u'amount': -10,
-                     u'date': u'2015-04-28',
-                     u'entityId': u'AD5F14BC-5BCC-E075-4B14-208676CA762F',
-                     u'accepted': True,
-                     u'payeeId': u'45C13591-718B-3025-0F3C-2086F37E7676',
-                     u'categoryId': u'DEF375CA-58D2-D332-4C79-20862B7566F8',
-                     u'accountId': u'37ADA60C- BE54-074E-F1B2-1FC8F2BE93CF'}]
+    this_transactions = [
+        {
+            u'cleared': u'Cleared',
+            u'entityVersion': u'A-118',
+            u'entityType': u'transaction',
+            u'memo': u'Loan for tea',
+            u'amount': -10,
+            u'date': u'2015-04-28',
+            u'entityId': u'AD5F14BC-5BCC-E075-4B14-208676CA762F',
+            u'accepted': True,
+            u'payeeId': u'45C13591-718B-3025-0F3C-2086F37E7676',
+            u'categoryId': u'DEF375CA-58D2-D332-4C79-20862B7566F8',
+            u'accountId': u'37ADA60C- BE54-074E-F1B2-1FC8F2BE93CF'
+        },
+        {
+            u'cleared': u'Cleared',
+            u'entityVersion': u'A-118',
+            u'entityType': u'transaction',
+            u'memo': u'Loan for nachos',
+            u'amount': -8,
+            u'date': u'2015-04-28',
+            u'entityId': u'AD5F14BC-5BCC-E075-4B14-208676CA762F',
+            u'accepted': True,
+            u'payeeId': u'45C13591-718B-3025-0F3C-2086F37E7676',
+            u'categoryId': u'DEF375CA-58D2-D332-4C79-20862B7566F8',
+            u'accountId': u'37ADA60C- BE54-074E-F1B2-1FC8F2BE93CF'
+        }
+    ]
 
-    other_transactions = [{u'cleared': u'Cleared',
-                     u'entityVersion': u'A-118',
-                     u'entityType': u'transaction',
-                     u'memo': u'Borrow for nachos',
-                     u'amount': 8,
-                     u'date': u'2015-04-28',
-                     u'entityId': u'AD5F14BC-5BCC-E075-4B14-208676CA762F',
-                     u'accepted': True,
-                     u'payeeId': u'45C13591-718B-3025-0F3C-2086F37E7676',
-                     u'categoryId': u'DEF375CA-58D2-D332-4C79-20862B7566F8',
-                     u'accountId': u'37ADA60C- BE54-074E-F1B2-1FC8F2BE93CF'}]
+    other_transactions = [
+        {
+            u'cleared': u'Cleared',
+            u'entityVersion': u'A-118',
+            u'entityType': u'transaction',
+            u'memo': u'Borrow for nachos',
+            u'amount': 8,
+            u'date': u'2015-04-28',
+            u'entityId': u'AD5F14BC-5BCC-E075-4B14-208676CA762F',
+            u'accepted': True,
+            u'payeeId': u'45C13591-718B-3025-0F3C-2086F37E7676',
+            u'categoryId': u'DEF375CA-58D2-D332-4C79-20862B7566F8',
+            u'accountId': u'37ADA60C- BE54-074E-F1B2-1FC8F2BE93CF'
+        }
+    ]
 
     budget_comparer.this_budget.data["transactions"] = this_transactions
     budget_comparer.other_budget.data["transactions"] = other_transactions
