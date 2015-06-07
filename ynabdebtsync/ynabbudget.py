5ad0bdbdb7e51d68d10b9c4ec0fd3d299c047fde
John Fisher <juanpescador@users.noreply.github.com>
2015-06-07 18:51:14 +0200

    Fix bug when advancing past missing transactions
    If the transaction that triggered a detection of missing transactions
    isn't the first transaction in the group of transactions for a given
    amount, using len(missing_transactions) to advance results in skipping
    transactions. E.g.
    
    ([-2, -2, -2, -1.8], [2, 1.8])
           ^
    
    The second -2 transaction would detect there are two transactions
    missing. In some cases, there could be three or more missing, and the
    next transaction would be skipped past -1.8 to another quantity. Then
    transactions of amount [-1.8, 1.8] would be tallied, and they might
    match, throwing a YnabBudgetComparerValueError.
    
    If missing transactions

--- ynabdebtsync/ynabbudget.py
+++ ynabdebtsync/ynabbudget.py
@@ -253,7 +253,10 @@ class YnabBudgetComparer:
                     if abs(this_amount) > abs(other_amount):
                         missing_transactions = self._get_this_budget_transactions_missing_from_other_budget(this_amount)
                         other_missing_transactions.extend(missing_transactions)
-                        for transaction in missing_transactions:
+                        # Advance to the next group of transactions. Advancing len(missing_transactions)
+                        # times is incorrect, as the starting transaction may not be the first
+                        # in the group of transactions for that amount, so we'd advance too far.
+                        while this_transaction["amount"] == this_amount:
                             this_transaction = next(this_iter, done)
 
                     # Don't need to check for abs(this_amount) == abs(other_amount).
@@ -262,31 +265,31 @@ class YnabBudgetComparer:
                     else:
                         missing_transactions = self._get_other_budget_transactions_missing_from_this_budget(other_amount)
                         this_missing_transactions.extend(missing_transactions)
-                        for transaction in missing_transactions:
+                        while other_transaction["amount"] == other_amount:
                             other_transaction = next(other_iter, done)
 
                 elif (this_amount < 0 and other_amount < 0):
                     missing_transactions = self._get_this_budget_transactions_missing_from_other_budget(this_amount)
                     other_missing_transactions.extend(missing_transactions)
-                    for transaction in missing_transactions:
+                    while this_transaction["amount"] == this_amount:
                         this_transaction = next(this_iter, done)
 
                 elif (this_amount > 0 and other_amount > 0):
                     missing_transactions = self._get_other_budget_transactions_missing_from_this_budget(other_amount)
                     this_missing_transactions.extend(missing_transactions)
-                    for transaction in missing_transactions:
+                    while other_transaction["amount"] == other_amount:
                         other_transaction = next(other_iter, done)
 
                 elif (this_amount > 0 and other_amount < 0):
                     if abs(this_amount) > abs(other_amount):
                         missing_transactions = self._get_other_budget_transactions_missing_from_this_budget(other_amount)
                         this_missing_transactions.extend(missing_transactions)
-                        for transaction in missing_transactions:
+                        while other_transaction["amount"] == other_amount:
                             other_transaction = next(other_iter, done)
                     else:
                         missing_transactions = self._get_this_budget_transactions_missing_from_other_budget(this_amount)
                         other_missing_transactions.extend(missing_transactions)
-                        for transaction in missing_transactions:
+                        while this_transaction["amount"] == this_amount:
                             this_transaction = next(this_iter, done)
 
 
