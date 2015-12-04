07b53da1a44d1a41d7a4ac9998d45386c5e6743d
John Fisher <juanpescador@users.noreply.github.com>
2015-12-04 18:56:05 +0100

    Test Flask session

--- ynabdebtsync/app.py
+++ ynabdebtsync/app.py
@@ -1,8 +1,13 @@
 # -*- coding: utf8 -*-
 
 from . import api
-from flask import render_template
+from flask import render_template, session, request, redirect, url_for
 
-@api.flask_app.route('/')
+@api.flask_app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        session['dropbox_token'] = request.form['other_target_category']
+        return redirect(url_for('index'))
     return render_template('index.html')
+
+api.flask_app.secret_key = 'secret key'
