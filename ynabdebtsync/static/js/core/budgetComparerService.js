1b715ccd796b63fae42b7eefd737971f616e821b
John Fisher <juanpescador@users.noreply.github.com>
2015-12-25 22:37:21 +0100

    Move budgetComparerService to core module

--- ynabdebtsync/static/js/core/budgetComparerService.js
+++ ynabdebtsync/static/js/core/budgetComparerService.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app')
+        .module('app.core')
         .factory('budgetComparerService', budgetComparerService);
 
     budgetComparerService.$inject = ['$http'];
