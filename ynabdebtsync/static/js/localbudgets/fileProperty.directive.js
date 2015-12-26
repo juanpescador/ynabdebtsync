c180812149a14a49c85a92539e634abc987ff6b8
John Fisher <juanpescador@users.noreply.github.com>
2015-12-26 03:15:07 +0100

    Replace custom expression parsing with $parse
    $parse is built into Angular and does the same thing, probably better.

--- ynabdebtsync/static/js/localbudgets/fileProperty.directive.js
+++ ynabdebtsync/static/js/localbudgets/fileProperty.directive.js
@@ -5,35 +5,21 @@
         .module('app.localbudgets')
         .directive('fileProperty', fileProperty);
 
+    fileProperty.$inject = ['$parse']
+
     // Modified from http://stackoverflow.com/a/19647381
-    function fileProperty() {
+    function fileProperty($parse) {
         // When the file input value changes, set the scope variable specified in the
         // attribute value to the file contents.
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
                 var onChangeHandler = scope.$eval(attrs.fileOnChange);
-                // If attrs.fileProperty points to a descendant object of
-                // scope, as is the case with "vm.property", we need
-                // to iterate through the hierarchy up to the object that holds
-                // the property. This avoids creating a property on scope whose
-                // name is the hierarchy, e.g.
-                // scope['vm.property'] instead of scope['vm']['property'].
-                var targetObject = scope;
-                var objectHierarchy = attrs.fileProperty.split('.');
-
-                // Array.prototype.pop() returns the last element. As we want
-                // to traverse the hierarchy from top to bottom, reverse the
-                // array so the first element popped is the one highest in the
-                // hierarchy.
-                objectHierarchy.reverse();
-                while (objectHierarchy.length > 1) {
-                    targetObject = targetObject[objectHierarchy.pop()];
-                }
-                var targetProperty = objectHierarchy.pop();
 
                 element.bind('change', function(event) {
-                    targetObject[targetProperty] = event.target.files[0];
+                    var getter = $parse(attrs.fileProperty);
+                    var setter = getter.assign;
+                    setter(scope, event.target.files[0]);
                 });
             }
         };
