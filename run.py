8ae8122942d2668b1e7186e592e9aabb95230d0b
John Fisher <juanpescador@users.noreply.github.com>
2015-12-28 17:42:30 +0100

    Update Heroku deploy configuration
    Due to the change in wiring up the server, both the api and the app
    modules need to be imported before running the Flask server, as they
    add functionality to it (API routes and view routes).
    
    Gunicorn only takes a module name and one of its variables as the
    argument, so a module is needed that does the needed imports.
    
    Gunicorn imports the module specified rather than executing it, so
    __name__ will never be '__main__'. Taking advantage of that, run.py can
    be used for both local debug and Heroku deployment (where debug is set
    to False).

--- run.py
+++ run.py
@@ -3,5 +3,13 @@
 import ynabdebtsync.server
 import ynabdebtsync.app
 import ynabdebtsync.api
+
 if __name__ == "__main__":
     ynabdebtsync.server.flask_app.run(debug=True)
+else:
+    # We're on Heroku, expose production mode flask_app to gunicorn. We can't
+    # just pass ynabdebtsync.server:flask_app to gunicorn because
+    # ynabdebtsync.app and .api need to be imported to add the different routes
+    # to the server.
+    heroku_server = ynabdebtsync.server.flask_app
+    heroku_server.debug = False
